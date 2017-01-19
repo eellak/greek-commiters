b5844bc21e9999b9f25076507b6abd8c2d029ec6
Polichronucci <nick@discloud.eu>
2017-01-19 14:16:09 +0200

    Change "." to source for cleaner code

--- main.sh
+++ main.sh
@@ -6,8 +6,8 @@ dir="$(pwd)"
 ## Create required dirs
 mkdir -p user/repos user/contr user/info orgs/repos orgs/contr
 
-. "${dir}/get_users.sh"
+source "${dir}/get_users.sh"
 
-. "${dir}/get_users_repos.sh"
+source "${dir}/get_users_repos.sh"
 
-. "${dir}/get_repo_info.sh"
+source "${dir}/get_repo_info.sh"
